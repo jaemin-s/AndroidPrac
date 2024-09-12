1cf5ac6a35f61585b81a99ff4e0c98f6886a9f2f
jaemin <jaemin4033@gmail.com>
2024-09-12 09:20:41 +0900

    fix: clean unused file

--- app/src/main/java/com/example/prac05app/MainActivity.kt
+++ app/src/main/java/com/example/prac05app/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.prac05app
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.prac05app.listViewPage.ListViewActivity
 import com.example.prac05app.recycleViewPage.RecycleActivity
 
@@ -19,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         val btnListView: Button = findViewById(R.id.btn_go_lv)
         val btnRecycleView: Button = findViewById(R.id.btn_go_rv)
 
-
         btnListView.setOnClickListener {
             val intent = Intent(this, ListViewActivity::class.java)
             startActivity(intent)
@@ -29,53 +24,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RecycleActivity::class.java)
             startActivity(intent)
         }
-
-        //ListView
-//        var listView: ListView = findViewById(R.id.lv)
-//
-//        val dummyArray = arrayOf("캘린더", "템플릿", "휴지통", "도움말과 지원")
-//
-//        val arrayAdapter: ArrayAdapter<*>
-//
-//        arrayAdapter = ArrayAdapter(
-//            this,
-//            R.layout.list_item,
-//            R.id.tv,
-//            dummyArray
-//        )
-//
-//        listView.adapter = arrayAdapter
-
-        //RecyclerView
-//        val recyclerView: RecyclerView = findViewById(R.id.rv)
-//        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-//
-//        var menuList: ArrayList<MenuModel> = arrayListOf(
-//            MenuModel("Menu1"),
-//            MenuModel("Menu2"),
-//            MenuModel("Menu3"),
-//            MenuModel("Menu4"),
-//            MenuModel("Menu1"),
-//            MenuModel("Menu2"),
-//            MenuModel("Menu3"),
-//            MenuModel("Menu4"),
-//            MenuModel("Menu1"),
-//            MenuModel("Menu2"),
-//            MenuModel("Menu3"),
-//            MenuModel("Menu4"),
-//            MenuModel("Menu1"),
-//            MenuModel("Menu2"),
-//            MenuModel("Menu3"),
-//            MenuModel("Menu4"),
-//            MenuModel("Menu1"),
-//            MenuModel("Menu2"),
-//            MenuModel("Menu3"),
-//            MenuModel("Menu4"),
-//        )
-//
-//        val adapter = MenuAdapter(menuList)
-//        recyclerView.adapter = adapter
-
 
     }
 }
--- app/src/main/java/com/example/prac05app/MenuModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.prac05app
-
-data class MenuModel(val name: String)
